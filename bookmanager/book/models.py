6bf8286a3722c542259d491f47a00eecde34e615
fan9fan <zhuche99@126.com>
2022-06-16 14:34:27 +0800

    db created

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -8,10 +8,22 @@ from django.db import models
 
 '''
 
-# 定义书类
+# 定义书类-->在数据库里的表名
 class BookInfo(models.Model):
     # id--->系统有自动定义，无须手段定义id
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, unique=20)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    # 修改表名要在类里重新定义一个类，即新表名
+    #类的固定写法 class Mete:
+    # 表名字的固定写法db_table='表名'
+    class Mete:
+        db_table='bookinfo'
+        verbose_name = '书籍管理' # 即admin
+
     #重写str方法，让admin显示书籍名字
     def __str__(self):
         return self.name
@@ -19,7 +31,24 @@ class BookInfo(models.Model):
 
 # 定义人物类
 class pepleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    gender = models.BooleanField()
-    # 外键的约束，人物属于哪本书
+    # 性别两种选项值，可定义一个有序字典
+    GENDER_CHOICE = (
+        (1,'male'),
+        (2,'female')
+    )
+    name = models.CharField(max_length=20, unique=True, verbose_name='姓名')
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+
+    # 外键的约束，读者属于书的下一类，外键约束选 级联CASCADE---->父表中删除或更新对应的行,同时自动的删除或更新自表中匹配的行
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    # 修改表名
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '读者'
+
+        # 重写str方法，让admin显示书籍名字
+    def __str__(self):
+        return self.name
